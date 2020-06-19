9866888ffd57ccc003352460bc63561cf3aaedc0
leonardodma <leonardodma@al.insper.edu.br>
2020-06-18 21:35:06 -0300

    Versão Final

--- assets/js/rename.js
+++ assets/js/rename.js
@@ -1,11 +1,15 @@
 function saveEdits(){
       var editElem = document.getElementById('task1');
+      var editElem = document.getElementById('task2');
       var userVersion = editElem.innerHTML;
       localStorage.setItem('label', userVersion)
       document.getElementById("update").innerHTML="Edits Saved!";
     }
 
     function checkEdits(){
-      if(localStorage.userEdits != null)
-        document.getElementById("task1").innerHTML=localStorage.getItem('label');
+    	if(localStorage.userEdits != null)
+        	document.getElementById("task1").innerHTML=localStorage.getItem('label');
+
+        if(localStorage.userEdits != null)
+        document.getElementById("task2").innerHTML=localStorage.getItem('label');
     }
